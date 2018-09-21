c9e66894a40dc9e4ec76b34534c6fd61bff9ba03
sanhaowuai <sanhaowuai@163.com>
2018-09-21 08:37:50 +0800

    最后提交

--- servlet_project/servlet_02_02/src/main/java/com/jimengjie/helper/DatabaseHelper.java
+++ servlet_project/servlet_02_02/src/main/java/com/jimengjie/helper/DatabaseHelper.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-/**
+/**D
  * 数据库助手类
  */
 public final class DatabaseHelper {
